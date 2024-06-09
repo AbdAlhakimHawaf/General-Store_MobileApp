b4624d80c839a5d983fd8b432b517d91bdc83961
Abd Alhakim Hawaf <abdalhakim.hawaf@gmail.com>
2024-06-09 19:05:11 +0300

    IOS Automation on MAC only

--- src/test/java/steps/D05_FinalizePurchaseStepDef.java
+++ src/test/java/steps/D05_FinalizePurchaseStepDef.java
@@ -1,6 +1,7 @@
 package steps;
 
 import activities.A03_CartTab;
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import io.cucumber.java.en.And;
@@ -9,6 +10,9 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 import static base.TestBase.driver;
 
@@ -33,5 +37,15 @@ public class D05_FinalizePurchaseStepDef {
     @Then("User Is Redirected to Browser")
     public void userIsRedirectedToBrowser() {
         CartTabObject.ClickOnVisitToButton();
+        ////android.widget.EditText // sendkeys //I Suppose it is Double Click Gesture
+        ////android.widget.Button[@text="بحث Google‏"] // Search Button // if it is not working then use Enter Keyboard
+        Set<String> Contexts = driver.getContextHandles();
+        for(String Context : Contexts){
+            System.out.println(Context);
+        }
+    //driver.context();
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText")).sendKeys("General-Store.Apk");
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"بحث Google\u200F\"]")).click();
+
     }
 }
